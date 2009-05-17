e9d8d5d999833773d00ac1dc3a2d26adf61ba7b6
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2009-05-17 14:16:31 +1000

    Finished au.tpg

--- source/providers/au/tpg.py
+++ source/providers/au/tpg.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import re
 import urllib
 
 from isping import helpers
@@ -7,7 +7,14 @@ from isping.exceptions import InvalidCredentialsException
 
 name = 'TPG Internet'
 url = 'http://tpg.com.au'
+
 account_url = 'https://cyberstore.tpg.com.au/your_account/index.php'
+
+reo_peak = re.compile(
+    'Peak Download used: (?P<peak>[\d\.]+) MB'
+    '.*'
+    'Off-Peak Download used: (?P<offpeak>[\d\.]+) MB'
+)
 
 def periods():
     return [
@@ -28,7 +35,6 @@ def get_usage():
     args = urllib.urlencode({
         'check_username': username,
         'password': password,
-#        'Submit': 'GO!',
     })
     html = opener.open(account_url, args).read()
     if html.find('Invalid') != -1:
@@ -36,5 +42,13 @@ def get_usage():
 
     # Fetch usage page
     html = opener.open(account_url + '?function=checkaccountusage').read()
-    helpers.view_in_browser(html)
+#    helpers.view_in_browser(html)
 
+    # Parse the usage page
+    match = reo_peak.search(html)
+    groups = match.groupdict('peak')
+
+    return [
+        groups['peak'],
+        groups['offpeak']
+    ]
